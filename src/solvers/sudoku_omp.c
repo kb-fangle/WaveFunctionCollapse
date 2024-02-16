07e31a68332c2c452a58c27393a7b698d59021bc
kb-fangle <153212789+kb-fangle@users.noreply.github.com>
2024-02-16 11:39:14 +0100

    OMP
    Création d'une version omp pour les fonctions:
    - grd_min_entropy : Un thread par blocks cherche le min entropy
    - propagate: propagation dans block, ligne et colonne en même temps (surement source de problème)
    - blk_propagate: chaque case en parallèle
    - grd_propagate_row & col: chaque bloc et chaque ligne/colonne en parallèle
    - check_grid: tous les checks en même temps
    - blk_check & check_row/col : même parallélisation que pour la propagation

--- src/solvers/sudoku_omp.c
+++ src/solvers/sudoku_omp.c
@@ -8,5 +8,30 @@
 bool
 solve_openmp(wfc_blocks_ptr blocks)
 {
-    return false;
+
+    uint64_t iteration  = 0;
+
+    entropy_location min_entropy_loc;
+    bool changed = true;
+
+    while (changed && (min_entropy_loc = grd_min_entropy_omp(blocks)).location.x != UINT32_MAX) {
+        const uint32_t gx = min_entropy_loc.location.gx;
+        const uint32_t gy = min_entropy_loc.location.gy;
+        const uint32_t x = min_entropy_loc.location.x;
+        const uint32_t y = min_entropy_loc.location.y;
+
+        uint64_t* collapsed = blk_at(blocks, gx, gy, x, y);
+        *collapsed = entropy_collapse_state(*collapsed, gx, gy, x, y, blocks->seed, iteration);
+        
+        changed = propagate_omp(blocks, gx, gy, x, y);
+
+        if (!check_grid_omp(blocks)) {
+            fprintf(stderr, "The grid is in an invalid state\n");
+            exit(EXIT_FAILURE);
+        }
+
+        iteration++;
+    }
+    
+    return changed;
 }
