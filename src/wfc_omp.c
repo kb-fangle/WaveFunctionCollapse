5ea6b3a33da144b2299ad27b7cc65bcdf30f9f26
kb-fangle <153212789+kb-fangle@users.noreply.github.com>
2024-02-17 12:42:46 +0100

    Correction check_grid_omp

--- src/wfc_omp.c
+++ src/wfc_omp.c
@@ -15,6 +15,7 @@
 #include <errno.h>
 #include <string.h>
 #include <strings.h>
+#include <omp.h>
 
 
 entropy_location 
@@ -37,6 +38,9 @@ grd_min_entropy_omp(const wfc_blocks_ptr blocks)
         }
     }
 
+    // grd_print(stdout,blocks);
+    // fputc('\n', stdout);
+
     return min_entropy_loc;
 }
 
@@ -44,18 +48,20 @@ bool
 grd_check_block_errors_omp(wfc_blocks_ptr blocks) {
     const uint32_t block_size = blocks->block_side * blocks->block_side;
     bool err_blk = true;
-    #pragma omp parallel for
+
+    #pragma omp parallel for num_threads(blocks->grid_side)
     for (uint32_t gy = 0; gy < blocks->grid_side; gy++) {
-        
-        #pragma omp parallel for
         for (uint32_t gx = 0; gx < blocks->grid_side; gx++) {
             uint64_t* blk = grd_at(blocks, gx, gy);
+            // printf("Nombre de threads:%d\n",omp_get_num_threads());
+            // int tid = omp_get_thread_num();
+            // printf("block(%d,%d) in thread: %d\n",gx,gy,tid);
 
             // mask composed of all collapsed states
             uint64_t collapsed_mask = 0;
             for (uint32_t i = 0; i < block_size; i++) {
                 if (blk[i] == 0 || (blk[i] & collapsed_mask) != 0) {
-                    fprintf(stderr,"Failed block check\n");
+                    fprintf(stderr,"Failed check in block(%d,%d,%d)\n",gx,gy,i);
                     err_blk = false;
                     // return false; // TODO: Afficher un message d'erreur ?
                 }
@@ -75,12 +81,13 @@ grd_check_block_errors_omp(wfc_blocks_ptr blocks) {
 bool
 grd_check_row_errors_omp(wfc_blocks_ptr blocks) {
     bool err_row = true;
-    #pragma omp parallel for
+    #pragma omp parallel for num_threads(blocks->grid_side)
     for (uint32_t gy = 0; gy < blocks->grid_side; gy++) {
-        
-        #pragma omp parallel for
         for (uint32_t y = 0; y < blocks->block_side; y++) {
             uint64_t collapsed_mask = 0;
+            // printf("Nombre de threads:%d\n",omp_get_num_threads());
+            // int tid = omp_get_thread_num();
+            // printf("row(%d,%d) in thread: %d\n",gy,y,tid);
 
             for (uint32_t gx = 0; gx < blocks->grid_side; gx++) {
                 uint64_t* row = blk_at(blocks, gx, gy, 0, y);
@@ -109,12 +116,13 @@ bool
 grd_check_column_errors_omp(wfc_blocks_ptr blocks) {
     
     bool err_col = true;
-    #pragma omp parallel for
-    for (uint32_t gx = 0; gx < blocks->grid_side; gx++) {
-        
-        #pragma omp parallel for
+    #pragma omp parallel for num_threads(blocks->grid_side)
+    for (uint32_t gx = 0; gx < blocks->grid_side; gx++) {     
         for (uint32_t x = 0; x < blocks->block_side; x++) {
             uint64_t collapsed_mask = 0;
+            // printf("Nombre de threads:%d\n",omp_get_num_threads());
+            // int tid = omp_get_thread_num();
+            // printf("col(%d,%d) in thread: %d\n",gx,x,tid);
         
             for (uint32_t gy = 0; gy < blocks->grid_side; gy++) {
                 uint64_t* col = blk_at(blocks, gx, gy, x, 0);
@@ -144,15 +152,124 @@ check_grid_omp(const wfc_blocks_ptr blocks)
     bool err_blk = true;
     bool err_row = true;
     bool err_col = true;
-    #pragma omp parallel sections
+
+    // omp_set_num_threads(3);
+    // #pragma omp parallel  
+    // {
+    //     #pragma omp sections nowait
+    //     {
+    //     #pragma omp section 
+    //     err_blk = grd_check_block_errors(blocks);
+    //     #pragma omp section 
+    //     err_row = grd_check_row_errors(blocks);
+    //     #pragma omp section
+    //     err_col = grd_check_column_errors(blocks);
+    //     }
+    // }
+
+    // omp_set_num_threads(3);
+    // #pragma omp   
+    // {
+    //     #pragma omp sections nowait
+    //     {
+    //     #pragma omp section 
+    //     err_blk = grd_check_block_errors_omp(blocks);
+    //     #pragma omp section 
+    //     err_row = grd_check_row_errors_omp(blocks);
+    //     #pragma omp section
+    //     err_col = grd_check_column_errors_omp(blocks);
+    //     }
+    // }
+    
+    
+
+    const uint32_t block_size = blocks->block_side * blocks->block_side;
+
+    omp_set_num_threads(blocks->grid_side);
+    #pragma omp parallel
     {
-        #pragma omp section
-        err_blk = grd_check_block_errors_omp(blocks);
-        #pragma omp section
-        err_row = grd_check_row_errors_omp(blocks);
-        #pragma omp section
-        err_col = grd_check_column_errors_omp(blocks);
+    #pragma omp for nowait schedule(runtime)
+    for (uint32_t gy = 0; gy < blocks->grid_side; gy++) {
+        for (uint32_t gx = 0; gx < blocks->grid_side; gx++) {
+            uint64_t* blk = grd_at(blocks, gx, gy);
+            // printf("Nombre de threads:%d\n",omp_get_num_threads());
+            // int tid = omp_get_thread_num();
+            // printf("block(%d,%d) in thread: %d\n",gx,gy,tid);
+
+            // mask composed of all collapsed states
+            uint64_t collapsed_mask = 0;
+            for (uint32_t i = 0; i < block_size; i++) {
+                if (blk[i] == 0 || (blk[i] & collapsed_mask) != 0) {
+                    fprintf(stderr,"Failed check in block(%d,%d,%d)\n",gx,gy,i);
+                    err_blk = false;
+                    // return false; // TODO: Afficher un message d'erreur ?
+                }
+
+                if (entropy_compute(blk[i]) == 1) {
+                    collapsed_mask |= blk[i];
+                }
+            }
+        }
     }
+
+    #pragma omp for nowait collapse(2) schedule(runtime)
+    for (uint32_t gy = 0; gy < blocks->grid_side; gy++) {
+        for (uint32_t y = 0; y < blocks->block_side; y++) {
+            uint64_t collapsed_mask = 0;
+            // printf("Nombre de threads:%d\n",omp_get_num_threads());
+            // int tid = omp_get_thread_num();
+            // printf("row(%d,%d) in thread: %d\n",gy,y,tid);
+
+            for (uint32_t gx = 0; gx < blocks->grid_side; gx++) {
+                uint64_t* row = blk_at(blocks, gx, gy, 0, y);
+
+                for (uint32_t x = 0; x < blocks->block_side; x++) {
+                    if (row[x] == 0 || (row[x] & collapsed_mask) != 0) {
+                        fprintf(stderr,"Failed row check\n");
+                        // return false;
+                        err_row = false;
+                    }
+
+                    if (entropy_compute(row[x]) == 1) {
+                        collapsed_mask |= row[x];
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    #pragma omp for nowait collapse(2) schedule(runtime)
+    for (uint32_t gx = 0; gx < blocks->grid_side; gx++) {     
+        for (uint32_t x = 0; x < blocks->block_side; x++) {
+            uint64_t collapsed_mask = 0;
+            // printf("Nombre de threads:%d\n",omp_get_num_threads());
+            // int tid = omp_get_thread_num();
+            // printf("col(%d,%d) in thread: %d\n",gx,x,tid);
+        
+            for (uint32_t gy = 0; gy < blocks->grid_side; gy++) {
+                uint64_t* col = blk_at(blocks, gx, gy, x, 0);
+
+                for (uint32_t y = 0; y < blocks->block_side; y++) {
+                    const uint64_t state = col[y * blocks->block_side];
+                    if (state == 0 || (state & collapsed_mask) != 0) {
+                        fprintf(stderr,"Failed column check\n");
+                        // return false;
+                        err_col = false;
+                    }
+
+                    if (entropy_compute(state) == 1) {
+                        collapsed_mask |= state;
+                    }
+                }
+            }
+        }
+    }
+
+    }
+
+
     return err_blk && err_row && err_col;
 }
 
@@ -164,17 +281,20 @@ blk_propagate_omp(wfc_blocks_ptr blocks,
     uint64_t* block_loc = grd_at(blocks,gx,gy);
     bool changed = false;
 
-    #pragma omp parallel for shared(collapsed_stack)
-    for (uint32_t i = 0; i < blocks->block_side * blocks->block_side; i++) {
-        const uint64_t new_state = block_loc[i] & ~collapsed;
+    #pragma omp parallel  
+    {
+        #pragma omp for
+        for (uint32_t i = 0; i < blocks->block_side * blocks->block_side; i++) {
+            const uint64_t new_state = block_loc[i] & ~collapsed;
 
-        changed |= new_state != 0 && new_state != block_loc[i];
+            changed |= new_state != 0 && new_state != block_loc[i];
 
-        if (new_state != block_loc[i] && bitfield_count(new_state) == 1) {
-            position pos = { gx, gy, i % blocks->block_side, i / blocks->block_side };
-            position_list_push(collapsed_stack, pos);
+            if (new_state != block_loc[i] && bitfield_count(new_state) == 1) {
+                position pos = { gx, gy, i % blocks->block_side, i / blocks->block_side };
+                position_list_push(collapsed_stack, pos);
+            }
+            block_loc[i] = new_state;
         }
-        block_loc[i] = new_state;
     }
 
     return changed;
@@ -187,10 +307,10 @@ grd_propagate_row_omp(wfc_blocks_ptr blocks, uint32_t gy, uint32_t y,
     uint64_t* row = 0;
     bool changed = false;
 
-    #pragma omp parallel for 
+    #pragma omp parallel for schedule(static,blocks->grid_side)
     for (uint32_t gx=0; gx < blocks->grid_side;gx++){
         row = blk_at(blocks,gx,gy,0,y);
-        #pragma omp parallel for shared(collapsed_stack)
+        #pragma omp parallel for shared(collapsed_stack) schedule(static,blocks->block_side)
         for (uint32_t x=0; x < blocks->block_side;x++){
             const uint64_t new_state = row[x] & ~collapsed;
             
@@ -215,10 +335,10 @@ grd_propagate_column_omp(wfc_blocks_ptr blocks, uint32_t gx,
     uint64_t* col = 0;
     bool changed = false;
 
-    #pragma omp parallel for
+    #pragma omp parallel for schedule(static,blocks->grid_side)
     for (uint32_t gy=0; gy < blocks->grid_side;gy++){
         col = blk_at(blocks,gx,gy,x,0);
-        #pragma omp parallel for shared(collapsed_stack)
+        #pragma omp parallel for shared(collapsed_stack) schedule(static,blocks->block_side)
         for (uint32_t y=0; y < blocks->block_side;y++){
             const uint32_t index = y * blocks->block_side;
             const uint64_t new_state = col[index] & ~collapsed;
@@ -252,7 +372,7 @@ propagate_omp(wfc_blocks_ptr blocks, uint32_t gx, uint32_t gy, uint32_t x, uint3
         uint64_t* collapsed_cell = blk_at(blocks, pos.gx, pos.gy, pos.x, pos.y);
         const uint64_t collapsed = *collapsed_cell;
 
-        #pragma omp parallel sections shared(changed)
+        #pragma omp sections nowait
         {
             #pragma omp section
             changed |= blk_propagate_omp(blocks, pos.gx, pos.gy, collapsed, &collapsed_stack);
@@ -261,6 +381,7 @@ propagate_omp(wfc_blocks_ptr blocks, uint32_t gx, uint32_t gy, uint32_t x, uint3
             #pragma omp section
             changed |= grd_propagate_column_omp(blocks, pos.gx, pos.x, collapsed, &collapsed_stack);
         }
+        
 
         // The propagate functions will overwrite the collapsed state, so we
         // reset it to the right value
